a8dc597ee0040da932101bc79d15205d45a68205
tuanit168 <tuanit168@gmail.com>
2022-06-16 18:16:56 +0700

    Get the Server run with a specific Port

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,20 @@
-const test: string = "Hello Tuan Ngo";
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
-console.log(test);
+// Middleware
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+// server listenning
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
