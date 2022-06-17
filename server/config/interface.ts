84b389e44b82b258f6d1140d0764102326f19d57
tuanit168 <tuanit168@gmail.com>
2022-06-17 18:51:14 +0700

    05 - Login function then can get a Token in Cookies

--- server/config/interface.ts
+++ server/config/interface.ts
@@ -1,11 +1,23 @@
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  account: string;
+  password: string;
+  avatar: string;
+  role: string;
+  type: string;
+  _doc: object;
+}
+
 export interface INewUser {
-  name: string
-  account: string
-  password: string
+  name: string;
+  account: string;
+  password: string;
 }
 
 export interface IDecodedToken {
-  newUser?: INewUser
-  iat: number
-  exp: number
+  newUser?: INewUser;
+  iat: number;
+  exp: number;
 }
